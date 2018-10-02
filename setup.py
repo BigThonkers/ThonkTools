34479ad1a25feab3b8376b195b19145fdd3cdcdd
dellpit <dellpit@pm.me>
2018-10-02 13:22:53 +0200

    fixed setup.py to include readme

--- setup.py
+++ setup.py
@@ -1,3 +1,3 @@
 from setuptools import setup
 
-setup(name='THONKtools',version='0.1',description='Wrappers for ease of use for lab stuff',long_description=readme(),url='https://github.com/BigThonkers/THONKtools',author='BigThonkers/Patrick Munnich',author_email='munnich@protonmail.com',install_requires=['numpy','matplotlib','scipy'],packages=['THONKtools'],zip_safe=False)
+setup(name='THONKtools',version='0.1',description='Wrappers for ease of use for lab stuff',long_description='README.md',url='https://github.com/BigThonkers/THONKtools',author='BigThonkers/Patrick Munnich',author_email='munnich@protonmail.com',install_requires=['numpy','matplotlib','scipy'],packages=['THONKtools'],zip_safe=False)
