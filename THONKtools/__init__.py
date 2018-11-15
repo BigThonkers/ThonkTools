c19dd7da482bd9465d2630f5a1182cd0695f1256
shockcookie <34022104+shockcookie@users.noreply.github.com>
2018-11-15 13:24:21 +0100

    Finished helpers_2
    Integrated helpers_2 to init.

--- THONKtools/__init__.py
+++ THONKtools/__init__.py
@@ -1,5 +1,6 @@
 from THONKtools.linreg import linreg
 from THONKtools.mean import mean
 from THONKtools.helpers import *
+from THONKtools.helpers_2 import *
 from THONKtools.textools import *
 from THONKtools.x import *
