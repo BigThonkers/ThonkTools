325250b240b49fc5e9be7de8c4711fa91186d33a
dellpit <munnich@protonmail.com>
2019-02-27 13:27:49 +0100

    commented out helpers_2

--- ThonkTools/__init__.py
+++ ThonkTools/__init__.py
@@ -1,6 +1,6 @@
 from ThonkTools.linreg import linreg
 from ThonkTools.mean import mean
 from ThonkTools.helpers import *
-from ThonkTools.helpers_2 import *
+#from ThonkTools.helpers_2 import *
 from ThonkTools.textools import *
 from ThonkTools.x import *
