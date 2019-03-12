7c351f7b42475c0fcae7e2eb48529d7ca9ea1069
dellpit <munnich@protonmail.com>
2019-03-12 18:48:37 +0100

    fixed extractor (hopefully)

--- ThonkTools/extractor.py
+++ ThonkTools/extractor.py
@@ -1,14 +1,14 @@
 def excelrep(x, pmaxi=10, nmaxi=10):
     if pmaxi or nmaxi > 99:
         raise ValueError("Power limit reached.")
-    for i in pmaxi;
-    if i < 10:
-        x.replace("E+0{0}".format(i), "\times10^{0}".format(i))
-    else:
-        x.replace("E+{0}".format(i), "\times10^{0}".format(i))
-    for i in nmaxi;
-    if i < 10:
-        x.replace("E+0{0}".format(i), "\times10^{0}".format(i))
-    else:
-        x.replace("E+{0}".format(i), "\times10^{0}".format(i))
+    for i in pmaxi:
+        if i < 10:
+            x.replace("E+0{0}".format(i), "\times10^{0}".format(i))
+        else:
+            x.replace("E+{0}".format(i), "\times10^{0}".format(i))
+    for i in nmaxi:
+        if i < 10:
+            x.replace("E+0{0}".format(i), "\times10^{0}".format(i))
+        else:
+            x.replace("E+{0}".format(i), "\times10^{0}".format(i))
     return x
