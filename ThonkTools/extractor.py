b2b09eedf445c7324d023bbe1cb930468dab60c8
Patrick Munnich <38660244+dellpit@users.noreply.github.com>
2019-03-12 23:22:06 +0100

    doc and cdot

--- ThonkTools/extractor.py
+++ ThonkTools/extractor.py
@@ -1,14 +1,36 @@
-def excelrep(x, pmaxi=10, nmaxi=10):
+def excelrep(x, pmaxi=10, nmaxi=10, cdot=False):
+    """
+    Excel tractor
+    Convert Excel's notation to LaTeX.
+
+    :param x: Excel output as string
+    :param pmaxi: Max positive value
+    :param nmaxi: Max negative value
+    :param cdot: Whether to use \times or \cdot
+    :return: "10^{1}"
+    """
     if pmaxi or nmaxi > 99:
         raise ValueError("Power limit reached.")
-    for i in pmaxi:
-        if i < 10:
-            x.replace("E+0{0}".format(i), "\times10^{0}".format(i))
-        else:
-            x.replace("E+{0}".format(i), "\times10^{0}".format(i))
-    for i in nmaxi:
-        if i < 10:
-            x.replace("E+0{0}".format(i), "\times10^{0}".format(i))
-        else:
-            x.replace("E+{0}".format(i), "\times10^{0}".format(i))
+    if cdot == False:
+        for i in pmaxi:
+            if i < 10:
+                x.replace("E+0{0}".format(i), "\times10^{0}".format(i))
+            else:
+                x.replace("E+{0}".format(i), "\times10^{0}".format(i))
+        for i in nmaxi:
+            if i < 10:
+                x.replace("E+0{0}".format(i), "\times10^{0}".format(i))
+            else:
+                x.replace("E+{0}".format(i), "\times10^{0}".format(i))
+    else:
+        for i in pmaxi:
+            if i < 10:
+                x.replace("E+0{0}".format(i), "\cdot10^{0}".format(i))
+            else:
+                x.replace("E+{0}".format(i), "\cdot10^{0}".format(i))
+        for i in nmaxi:
+            if i < 10:
+                x.replace("E+0{0}".format(i), "\cdot10^{0}".format(i))
+            else:
+                x.replace("E+{0}".format(i), "\cdot10^{0}".format(i))
     return x
