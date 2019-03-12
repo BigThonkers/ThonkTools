40fa73def4a48b7a5e2a4cba3d1b3374a54abcc0
dellpit <munnich@protonmail.com>
2019-03-12 09:16:11 +0100

    fit function

---
+++ ThonkTools/fit.py
@@ -0,0 +1,97 @@
+def fit(x, y, func=lambda x: x, r=None, d=None):
+    """
+        fit takes data points calculates the curve fit and gives back
+        the values with which the curve can be plotted
+        Parameters:
+        -----------
+        x: x-value of the data points.
+        y: y-value of the data points.
+        func: the function type the curve_fit will be applied to.
+        r: determines the size of steps in which the x-achse intervall is split.
+        d: tuple which determines the start and end point of the x-achse intervall on which the
+            will be drawn. If not given the curve will be drawn between the largest and the smalest
+            given x-value.
+        Returns:
+        -----------
+        xnew: x-value with which the fitted curve can be plotted.
+        ynew: y-value with which the fitted curve can be plotted.
+    """
+    from scipy.optimize import curve_fit
+    popt, pvoc = curve_fit(func, x, y)
+    params = popt.copy()
+    if d == None:
+        if r == None:
+            xnew = np.arange(x[0], x[-1], np.abs((x[-1] - x[0]) / 100))
+        else:
+            xnew = np.arange(x[0], x[-1], r)
+    else:
+        if r == None:
+            xnew = np.arange(d[0], d[1], np.abs((x[-1] - x[0]) / 100))
+        else:
+            xnew = np.arange(d[0], d[1], r)
+    ynew = func(xnew, *params)
+    return xnew, ynew
+
+
+def expfit(x, y, r=None, d=None):
+    import numpy as np
+    return fit(x, y, func=lambda x, a, b, c: a * np.exp(b * x) + c, r=r, d=d)
+
+
+def linfit(x, y, r=None, d=None):
+    return fit(x, y, func=lambda x, a, b,: a * x + b, r=r, d=d)
+
+
+def grfit(x, y, r=None, d=None):
+    import numpy as np
+    return fit(x, y, func=lambda x, s, b, k: s - (s - b) * np.exp(-k * x), r=r, d=d)
+
+
+def logfit(x, y, r=None, d=None):
+    import numpy as np
+    return fit(x, y, func=lambda x, L, k, x_0: L / (1 + np.exp(-k * (x - x0))), r=r, d=d)
+
+
+def fit_pm(x, y, func=lambda x: x):
+    """
+        fit_pm takes data points and makes an exponential curve fit. It returns
+        the parameters with errors and the coefficient of determination
+        Parameters:
+        -----------
+        x: x-value of the data points.
+        y: y-value of the data points.
+        func: Function to be fitted to.
+        Returns:
+        -----------
+        list: Gives back a list with the parameters of an expotential function the
+              corresponding errors and the coefficient of determination.
+    """
+    from scipy.optimize import curve_fit
+    from uncertainties import unumpy as unp
+    popt, pcov = curve_fit(func, x, y)
+    params = popt.copy()
+    errors = np.sqrt(np.diag(pcov))
+    r = np.array(y) - func(np.array(x), *params)
+    rss = np.sum(r ** 2)
+    tss = np.sum((y - np.mean(y)) ** 2)
+    R_2 = 1 - (rss / tss)
+    return unp.uarray(params, errors), R_2
+
+
+def expfit_pm(x, y):
+    import numpy as np
+    return fit_pm(x, y, func=lambda x, a, b, c: a * np.exp(b * x) + c)
+
+
+def linfit_pm(x, y):
+    return fit_pm(x, y, func=lambda x, a, b,: a * x + b)
+
+
+def grfit_pm(x, y):
+    import numpy as np
+    return fit_pm(x, y, func=lambda x, s, b, k: s - (s - b) * np.exp(-k * x))
+
+
+def logfit_pm(x, y):
+    import numpy as np
+    return fit_pm(x, y, func=lambda x, L, k, x_0: L / (1 + np.exp(-k * (x - x0))))
